f1ed292f9663aa6268ec99986cd21329720c33c2
HJY <hwangjae1139@gmail.com>
2024-10-11 18:34:16 +0900

    fix: 불필요 코드 제거

--- src/hooks/blog/usePost.ts
+++ src/hooks/blog/usePost.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { supabase } from "@/utils/supabase";
 import { IPostsProps, IPostDetailProps } from "@/types";
 import dayjs from "dayjs";
@@ -80,20 +80,6 @@ const usePost = (id: any) => {
     }
     return true;
   };
-
-  const allFetch = async () => {
-    await getPostDetail();
-  };
-  // useEffect(() => {
-  //   try {
-  //     setUserLoading(true);
-  //     allFetch();
-  //   } catch (error: any) {
-  //     console.log(error);
-  //   } finally {
-  //     setUserLoading(false);
-  //   }
-  // }, [id]);
 
   return {
     getPostDetail,
