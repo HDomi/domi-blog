f4a7b46e30d9e21168d8567bac333e6a2865eddd
HJY <hwangjae1139@gmail.com>
2024-11-04 14:14:28 +0900

    feat: 비밀번호 hide/show 버튼의 눈동자가 따라오도록 수정

--- src/components/inputs/CustomInput.tsx
+++ src/components/inputs/CustomInput.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./input.module.scss";
-
+import IconEyeBall from "@/svgIcons/IconEyeBall";
+import IconEyeOpen from "@/svgIcons/IconEyeOpen";
+import IconEyeClose from "@/svgIcons/IconEyeClose";
+import CNJ from "@/utils/classNameJoiner";
+import cx from "clsx";
 interface CustomInputProps {
   value: string;
   placeholder: string;
@@ -22,18 +26,144 @@ const CustomInput: React.FC<CustomInputProps> = ({
   type = "text",
   onChange,
 }) => {
+  const [inputType, setInputType] = useState<string>(type);
+  const onClickPasswordTypeButton = () => {
+    setInputType(inputType === "password" ? "text" : "password");
+  };
+  const originRef = useRef<HTMLButtonElement>(null);
+  const [cord, setCord] = useState({
+    top: false,
+    right: false,
+    bottom: false,
+    left: false,
+  });
+  useEffect(() => {
+    const onMouseMove = (e: MouseEvent): void => {
+      if (!originRef.current) return;
+
+      const { x: x1, y: y1 } = e;
+      const { x: x2, y: y2 } = originRef.current.getBoundingClientRect();
+
+      let rad = Math.atan2(y2 - y1, x2 - x1);
+      if (rad < 0) rad += Math.PI * 2;
+      rad = (rad * 180) / Math.PI;
+      if (rad < 22.5) {
+        // L
+        setCord({
+          top: false,
+          right: false,
+          bottom: false,
+          left: true,
+        });
+      } else if (rad < 67.5) {
+        // TL
+        setCord({
+          top: true,
+          right: false,
+          bottom: false,
+          left: true,
+        });
+      } else if (rad < 112.5) {
+        // T
+        setCord({
+          top: true,
+          right: false,
+          bottom: false,
+          left: false,
+        });
+      } else if (rad < 157.5) {
+        // TR
+        setCord({
+          top: true,
+          right: true,
+          bottom: false,
+          left: false,
+        });
+      } else if (rad < 202.5) {
+        // R
+        setCord({
+          top: false,
+          right: true,
+          bottom: false,
+          left: false,
+        });
+      } else if (rad < 247.5) {
+        // BR
+        setCord({
+          top: false,
+          right: true,
+          bottom: true,
+          left: false,
+        });
+      } else if (rad < 292.5) {
+        // B
+        setCord({
+          top: false,
+          right: false,
+          bottom: true,
+          left: false,
+        });
+      } else if (rad < 337.5) {
+        // BL
+        setCord({
+          top: false,
+          right: false,
+          bottom: true,
+          left: true,
+        });
+      } else if (rad < 360) {
+        // L
+        setCord({
+          top: false,
+          right: false,
+          bottom: false,
+          left: true,
+        });
+      }
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  });
   return (
-    <input
-      className={style["custom-input"]}
-      value={value}
-      id={id}
-      required={required}
-      name={name}
-      type={type}
-      style={{ width: width }}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <div className={style["input-contents"]} style={{ width: width }}>
+      <input
+        className={style["custom-input"]}
+        value={value}
+        id={id}
+        required={required}
+        name={name}
+        type={inputType}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      {type === "password" && (
+        <button
+          type="button"
+          className={style["fancy-eye-ball"]}
+          onClick={onClickPasswordTypeButton}
+          ref={originRef}
+        >
+          {inputType === "password" ? (
+            <>
+              <IconEyeOpen className={style["eye-open"]} />
+              <IconEyeBall
+                className={cx(style["eye-ball"], {
+                  [style.top]: cord.top,
+                  [style.bottom]: cord.bottom,
+                  [style.left]: cord.left,
+                  [style.right]: cord.right,
+                })}
+              />
+            </>
+          ) : (
+            <IconEyeClose className={CNJ([style["eye-close"], cord.bottom])} />
+          )}
+        </button>
+      )}
+    </div>
   );
 };
 
---
+++ src/svgIcons/IconEyeBall.tsx
@@ -0,0 +1,18 @@
+const IconEyeBall = (className: any) => {
+  return (
+    <svg
+      id="Layer_2"
+      data-name="Layer 2"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 52 52"
+      className={className.className}
+    >
+      <defs></defs>
+      <path
+        style={{ fill: "#000", strokeWidth: "0px" }}
+        d="M26,17.746c-4.154,0-7.521,3.367-7.521,7.521s3.367,7.521,7.521,7.521,7.521-3.367,7.521-7.521-3.367-7.521-7.521-7.521ZM22.992,24.58c-.952,0-1.723-.772-1.723-1.723s.772-1.723,1.723-1.723,1.723.772,1.723,1.723-.772,1.723-1.723,1.723Z"
+      />
+    </svg>
+  );
+};
+export default IconEyeBall;
